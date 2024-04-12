f2ddd8e51500a28005dbe0a435bece8e7f436088
iwatkot <iwatkot@gmail.com>
2024-04-12 14:18:27 +0200

    Merge all.

--- src/merge_classes.py
+++ src/merge_classes.py
@@ -1,12 +1,15 @@
 import os
 import supervisely as sly
 from supervisely.app.v1.app_service import AppService
+from dotenv import load_dotenv
+
+load_dotenv("debug.env")
 
 my_app: AppService = AppService()
 
-TEAM_ID = int(os.environ['context.teamId'])
-WORKSPACE_ID = int(os.environ['context.workspaceId'])
-PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
+TEAM_ID = int(os.environ["context.teamId"])
+WORKSPACE_ID = int(os.environ["context.workspaceId"])
+PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
 ORIGINAL_META = None
 REMAIN_UNCHANGED = "remain unchanged"
@@ -19,7 +22,11 @@ SHAPE_TO_ICON = {
     sly.Polygon: {"icon": "icons8-polygon", "color": "#2cd26e", "bg": "#d8f8e7"},
     sly.AnyGeometry: {"icon": "zmdi zmdi-grain", "color": "#e09e11", "bg": "#faf0d8"},
     sly.Polyline: {"icon": "zmdi zmdi-minus", "color": "#ceadff", "bg": "#f6ebff"},
-    sly.Point: {"icon": "zmdi zmdi-dot-circle-alt", "color": "#899aff", "bg": "#edeeff"},
+    sly.Point: {
+        "icon": "zmdi zmdi-dot-circle-alt",
+        "color": "#899aff",
+        "bg": "#edeeff",
+    },
 }
 
 UNKNOWN_ICON = {"icon": "zmdi zmdi-shape", "color": "#ea9d22", "bg": "#fcefd9"}
@@ -41,15 +48,20 @@ def init_data_and_state(api: sly.Api):
             "name": obj_class.name,
             "color": sly.color.rgb2hex(obj_class.color),
             "shape": obj_class.geometry_type.geometry_name(),
-            "shapeIcon": SHAPE_TO_ICON.get(obj_class.geometry_type, UNKNOWN_ICON)
+            "shapeIcon": SHAPE_TO_ICON.get(obj_class.geometry_type, UNKNOWN_ICON),
         }
 
         possible_classes = [{"value": REMAIN_UNCHANGED, "label": REMAIN_UNCHANGED}]
         for dest_class in ORIGINAL_META.obj_classes:
-            if obj_class.name == dest_class.name or obj_class.geometry_type != dest_class.geometry_type:
+            if (
+                obj_class.name == dest_class.name
+                or obj_class.geometry_type != dest_class.geometry_type
+            ):
                 continue
             else:
-                possible_classes.append({"value": dest_class.name, "label": dest_class.name})
+                possible_classes.append(
+                    {"value": dest_class.name, "label": dest_class.name}
+                )
 
         sly.logger.debug("{!r} -> {}".format(obj_class.name, possible_classes))
 
@@ -62,7 +74,9 @@ def init_data_and_state(api: sly.Api):
     data["projectId"] = PROJECT_ID
     project = api.project.get_info_by_id(PROJECT_ID)
     data["projectName"] = project.name
-    data["projectPreviewUrl"] = api.image.preview_url(project.reference_image_url, 100, 100)
+    data["projectPreviewUrl"] = api.image.preview_url(
+        project.reference_image_url, 100, 100
+    )
     return data, state
 
 
@@ -77,26 +91,57 @@ def convert_annotation(ann: sly.Annotation, dst_meta, selectors):
     return ann.clone(labels=new_labels)
 
 
+merge_all = False
+merge_all_with = None
+
+
+@my_app.callback("mergeAll")
+def merge_all_changed(api: sly.Api, task_id, context, state, app_logger):
+    global merge_all, merge_all_with
+    merge_all = state["mergeAll"]
+    app_logger.info(f"Merge all set to {merge_all}")
+
+
+@my_app.callback("mergeAllClassSelected")
+def merge_all_class_selected(api: sly.Api, task_id, context, state, app_logger):
+    app_logger.info("Merge all was selected")
+    selected_class = state["mergeAllWith"]
+
+    selectors = state["selectors"]
+    for key in selectors:
+        if key == selected_class:
+            selectors[key] = REMAIN_UNCHANGED
+        else:
+            selectors[key] = selected_class
+
+    global merge_all_with
+    merge_all_with = selectors
+    app_logger.info(f"Merge all with set to {merge_all_with}")
+
+
 @my_app.callback("convert")
 @sly.timeit
 def convert(api: sly.Api, task_id, context, state, app_logger):
     api.task.set_field(task_id, "data.started", True)
 
-    TEAM_ID = int(os.environ['context.teamId'])
-    WORKSPACE_ID = int(os.environ['context.workspaceId'])
-    PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
+    TEAM_ID = int(os.environ["context.teamId"])
+    WORKSPACE_ID = int(os.environ["context.workspaceId"])
+    PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
     src_project = api.project.get_info_by_id(PROJECT_ID)
 
     if src_project.type != str(sly.ProjectType.IMAGES):
-        raise RuntimeError("Project {!r} has type {!r}. App works only with type {!r}"
-                           .format(src_project.name, src_project.type, sly.ProjectType.IMAGES))
+        raise RuntimeError(
+            "Project {!r} has type {!r}. App works only with type {!r}".format(
+                src_project.name, src_project.type, sly.ProjectType.IMAGES
+            )
+        )
 
     src_meta_json = api.project.get_meta(src_project.id)
     src_meta = sly.ProjectMeta.from_json(src_meta_json)
 
     dst_meta = src_meta.clone(obj_classes=sly.ObjClassCollection())
     need_action = False
-    selectors = state["selectors"]
+    selectors = state["selectors"] if not merge_all else merge_all_with
     for cls in src_meta.obj_classes:
         dst_name = selectors[cls.name]
         if dst_name == REMAIN_UNCHANGED:
@@ -109,46 +154,57 @@ def convert(api: sly.Api, task_id, context, state, app_logger):
 
     if need_action is False:
         fields = [
-            {
-                "field": "state.showWarningDialog",
-                "payload": True
-            },
+            {"field": "state.showWarningDialog", "payload": True},
             {
                 "field": "data.started",
                 "payload": False,
-            }
+            },
         ]
         api.task.set_fields(task_id, fields)
         return
 
-    dst_name = src_project.name if _SUFFIX in src_project.name else src_project.name + _SUFFIX
-    dst_project = api.project.create(src_project.workspace_id, dst_name, description=_SUFFIX,
-                                     change_name_if_conflict=True)
-    sly.logger.info('Destination project is created.',
-                    extra={'project_id': dst_project.id, 'project_name': dst_project.name})
+    dst_name = (
+        src_project.name if _SUFFIX in src_project.name else src_project.name + _SUFFIX
+    )
+    dst_project = api.project.create(
+        src_project.workspace_id,
+        dst_name,
+        description=_SUFFIX,
+        change_name_if_conflict=True,
+    )
+    sly.logger.info(
+        "Destination project is created.",
+        extra={"project_id": dst_project.id, "project_name": dst_project.name},
+    )
     api.project.update_meta(dst_project.id, dst_meta.to_json())
 
     total_progress = api.project.get_images_count(src_project.id)
     current_progress = 0
-    ds_progress = sly.Progress('Processing:', total_cnt=total_progress)
+    ds_progress = sly.Progress("Processing:", total_cnt=total_progress)
     for ds_info in api.dataset.get_list(src_project.id):
         dst_dataset = api.dataset.create(dst_project.id, ds_info.name)
         img_infos_all = api.image.get_list(ds_info.id)
 
         for img_infos in sly.batched(img_infos_all):
-            img_names, img_ids, img_metas = zip(*((x.name, x.id, x.meta) for x in img_infos))
+            img_names, img_ids, img_metas = zip(
+                *((x.name, x.id, x.meta) for x in img_infos)
+            )
 
             ann_infos = api.annotation.download_batch(ds_info.id, img_ids)
             anns = [sly.Annotation.from_json(x.annotation, src_meta) for x in ann_infos]
 
             new_anns = [convert_annotation(ann, dst_meta, selectors) for ann in anns]
 
-            new_img_infos = api.image.upload_ids(dst_dataset.id, img_names, img_ids, metas=img_metas)
+            new_img_infos = api.image.upload_ids(
+                dst_dataset.id, img_names, img_ids, metas=img_metas
+            )
             new_img_ids = [x.id for x in new_img_infos]
             api.annotation.upload_anns(new_img_ids, new_anns)
 
             current_progress += len(img_infos)
-            api.task.set_field(task_id, "data.progress", int(current_progress * 100 / total_progress))
+            api.task.set_field(
+                task_id, "data.progress", int(current_progress * 100 / total_progress)
+            )
             ds_progress.iters_done_report(len(img_infos))
 
     api.task.set_output_project(task_id, dst_project.id, dst_project.name)
@@ -167,7 +223,7 @@ def convert(api: sly.Api, task_id, context, state, app_logger):
         {
             "field": "data.resultProjectPreviewUrl",
             "payload": api.image.preview_url(res_project.reference_image_url, 100, 100),
-        }
+        },
     ]
     api.task.set_fields(task_id, fields)
     my_app.stop()
